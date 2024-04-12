0977e8701f5eefbe7c17f8277918377f0eca8613
sanjayypal <sanjayypal113@gmail.com>
2024-04-12 17:29:58 +0530

    add utils function

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection Failed",err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Database Connection 2nd method
+// import express from "express"
+// const app = express()
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERR : ",error);
+//             throw err
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.error("ERROR",error)
+//         throw err
+//     }
+// })()
