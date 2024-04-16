b21f835809d2af9b1f12c664491514eb30adbcb4
sanjayypal <sanjayypal113@gmail.com>
2024-04-16 13:28:16 +0530

    register user-1

---
+++ src/routes/user.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.contoller.js";
+
+const router = Router();
+
+router.route("/register").post(registerUser)
+
+
+export default router
